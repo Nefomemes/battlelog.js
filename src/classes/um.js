3784808485a2a7ecaab23b2c2eb83f115bd41d85
ForTombstone <nefothingy@hotmail.com>
2021-02-22 11:16:53 +0700

    Update um.js

--- src/classes/um.js
+++ src/classes/um.js
@@ -1,19 +1,5 @@
 
-const { User } = require("./user.js");
 
-class UsersManager {
-	constructor(client){
-		this.client = client;
-	}
-
-	fetch(...params){
-		return new User(this.client, ...params).fetch();
-	}
-
-	cache = new Map()
-}
-
-module.exports.UsersManager = UsersManager;
 
 const { User } = require("./user");
 const { BattlelogMap } = require("./blmap");
