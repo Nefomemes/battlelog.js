2b207c46beba123a32e9f112e52e13e70fdf79d4
Restyled.io <commits@restyled.io>
2021-02-26 02:54:10 +0000

    Restyled by clang-format

--- src/classes/blclient.js
+++ src/classes/blclient.js
@@ -1,46 +1,39 @@
 
 const utils = require("../utils/utils");
-const { GameClient } = require("./gameclient");
+const {GameClient} = require("./gameclient");
 /**
- 
+
  * Represents a Battlelog session.
  *
  * @class
  */
 
 class BattlelogClient {
-	type = "BattlelogClient"
-	
-	
+  type = "BattlelogClient"
 
+  /**
 
-/**
- 
- * Creates a new Battlelog session.
- *
- * @class
- * @param {object} options- The options used here.
- */
+   * Creates a new Battlelog session.
+   *
+   * @class
+   * @param {object} options- The options used here.
+   */
 
-	constructor(options = {}){
-		if(options && typeof options !== "object"){
-			throw Error(`Parameter 'options' is required to be an object.  While it is ${utils.getArticle(typeof options)} ${typeof options}.`);
-		}
+  constructor(options = {}) {
+    if (options && typeof options !== "object") {
+      throw Error(
+          `Parameter 'options' is required to be an object.  While it is ${
+              utils.getArticle(typeof options)} ${typeof options}.`);
+    }
+  }
 
+  /**
 
-	}
-	
-/**
- 
- * Creates a new GameClient instance.
- *
- * @param {...*} params - The params used to create the instance.
- */
-	game(...params) {
-		return new GameClient(this, ...params);
-
-	}
-	
+   * Creates a new GameClient instance.
+   *
+   * @param {...*} params - The params used to create the instance.
+   */
+  game(...params) { return new GameClient(this, ...params); }
 }
-	
+
 module.exports.BattlelogClient = BattlelogClient;
--- src/classes/soldiersmanager.js
+++ src/classes/soldiersmanager.js
@@ -1,5 +1,5 @@
-const { Soldier } = require("./soldier");
-const { BattlelogMap } = require("./blmap");
+const {Soldier} = require("./soldier");
+const {BattlelogMap} = require("./blmap");
 
 /**
  * Manages a user's soldiers.
@@ -10,28 +10,24 @@ class SoldiersManager {
   cache = new BattlelogMap();
   /**
    *
-   * @class 
+   * @class
    * @param {User} user - The user that owns this SoldiersManager instance.
    * @param {Array} [data] -  Raw array data of the user's soldiers.
    */
-  constructor(user, data) {
-    this.user = user;
-  }
+  constructor(user, data) { this.user = user; }
   /**
    * Add
    *
    * @function
    * @param {object} data - The data
-   * @param {boolean} [fetch] - Whether the data is a result of SoldiersManager#fetch
+   * @param {boolean} [fetch] - Whether the data is a result of
+   *     SoldiersManager#fetch
    * @returns {SoldiersManager} this
    */
   structureData(data, fetch) {
     for (let soldier of data) {
-      this.cache.structureData(
-        soldier.persona.personaId,
-        new Soldier(this.user, data, fetch),
-        fetch
-      );
+      this.cache.structureData(soldier.persona.personaId,
+                               new Soldier(this.user, data, fetch), fetch);
     }
 
     return this;
@@ -44,9 +40,8 @@ class SoldiersManager {
    * @returns {SoldiersManager} this
    */
   async fetch() {
-    var res = await this.user.axios.get(
-      `/user/overviewBoxStats/${this.user.userId}`
-    );
+    var res =
+        await this.user.axios.get(`/user/overviewBoxStats/${this.user.userId}`);
 
     this.structureData(res.data.soldiersBox, true);
 
