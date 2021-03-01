c252d72d864600667ca1c0084f8490d2e7a71f7a
ForTombstone <nefothingy@hotmail.com>
2021-03-01 14:41:44 +0700

    Update test.js

--- test.js
+++ test.js
@@ -10,5 +10,5 @@
 
 
   await user.soldiers.fetch();
-  console.log(await user.soldiers.cache.values(i => i)[0].fetch());
+  console.log(await user.soldiers.cache.entries.map(i => i[1])[0].fetch());
 })();
