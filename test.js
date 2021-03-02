0ce0f2f27628e3ad97af8debb3b2df6c469b28dc
Restyled.io <commits@restyled.io>
2021-03-02 08:06:07 +0000

    Restyled by prettier

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-(async function() {
+(async function () {
   const bl = require("./src/index.js");
 
   var client = bl();
@@ -8,7 +8,5 @@
   var user = await bf3.users.fetch("JackFrags");
 
   await user.soldiers.fetch();
-  console.log(user.soldiers.cache
-
-  )
+  console.log(user.soldiers.cache);
 })();
