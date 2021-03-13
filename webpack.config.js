e1c51dc5657f02c22cdaf63880ad3fdaa357bc22
Nefomemes <nefothingy@hotmail.com>
2021-03-13 19:02:08 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/index.js'],,
+  entry:  './src/index.js',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.min.js",
