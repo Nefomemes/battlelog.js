7a40801128b558c3cb4346624ac8bfb03c81cb46
Nefomemes <nefothingy@hotmail.com>
2021-03-19 11:49:51 +0000

    Changes

--- src/classes/forumcategory.js
+++ src/classes/forumcategory.js
@@ -1,4 +1,17 @@
+const utils = require("../utils/utils")
+
+/**
+ * Represents a Forum Category.
+ * 
+ * @class
+ */
 class ForumCategory {
+
+  /**
+   * 
+   * @param {ForumClient} forum 
+   * @param {object} data 
+   */
     constructor(forum, data){
       Object.defineProperty(this, 'client', {
         value: forum.client,
@@ -7,18 +20,26 @@ class ForumCategory {
 
       Object.defineProperty(this, 'forum', {
         value: forum,
-        
+        enumerable: false
       })
+
+
+      this.structureData(data);
     }
   
   /**
    * 
-   
+   * 
+   * @param {object} object - The object used to structure the instance.
    * @function
+   * @returns {ForumCategory}
    */
-    structureData(){
+    structureData(data){
+utils.structureData(this, data);
+return this;
     }
  
+    
 }
 
 module.exports.ForumCategory = ForumCategory;
