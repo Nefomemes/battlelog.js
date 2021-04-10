3f2dd9b9f56cd66082ca55e96cf3125029979094
Nefomemes <nefothingy@hotmail.com>
2021-04-10 02:51:44 +0000

    dev(refactor): Rewrite blmap structureData function
    [ci skip]

--- src/classes/blmap.js
+++ src/classes/blmap.js
@@ -14,17 +14,25 @@ class BattlelogMap extends Map {
    * @param {...*} structureDataStuff - The additional parameters to be passed
    *     to the key's structureData method.
    */
-  structureData(key, value, ...structureDataStuff) {
+  structureData(options) {
     if (!key) throw Error("key is not specified");
     if (!value) throw Error("value is not specified");
     // Easter egg! Reference to this article on DEV: https://dev.to/jmdejager/commit-or-vomit-cast-2b2g
       switch (true) {
-        case (key instanceof Map): {
-          key.forEach((k, v) =>
-            key.structureData(k, v)
+        case (options.map instanceof Map): {
+          args[0].forEach((k, v) =>
+            this.structureData(k, v, ...structureDataStuff)
           );
           
           break;
+        }
+        case (args[0] instanceof Array): {
+		for(let [key, value] of args[0]){
+		this.structureData()
+
+		}
+        	
+        	break;
         }
         default: {
     if (super.get(key)) super.get(key).structureData(value, ...structureDataStuff);
