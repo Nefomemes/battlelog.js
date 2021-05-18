cad50e53f83df6b3a9a18ac06764f21cc9ddd97f
Nefomemes <nefothingy@hotmail.com>
2021-05-18 11:08:59 +0700

    Finish all necessary typings work

--- src/classes/platoon.ts
+++ src/classes/platoon.ts
@@ -1,54 +1,68 @@
 import type { AxiosInstance } from "axios";
 import * as utils from "../utils";
 import type { GameClient } from './gameclient';
+/**
+ * Represents a platoon.
+ */
 export class Platoon {
-  
+  /**
+   * The id of the platoon.
+   */
   id: string;
 
   client: GameClient;
-
+  badgePath: string;
   axios: AxiosInstance;
   /**
    * Creates a new Platoon instance.
    *
-   * @class
-   * @param client
-   * @param data
+   * @param client - The client of this instance
+   * @param data - Initial data of this instance
    */
   constructor(client: GameClient, data: Platoon | string) {
-    if (data && typeof data === "string") {
+    if(data){
+      if (typeof data === "string") {
       this.id = data;
     } else {
       this.structureData(data);
     }
   }
+  }
   isFan: boolean;
   allowNewMember: boolean;
+
+
+  members: []
   badge: {
     '60': string,
     '320': string
     };
   /**
-   * Structure the class using the data provided.
-   *
-   * @param {object} data - The data used to structure the class
-   * @returns {User} the User
+   * Structure the platoon instance using the data provided.
+: [ ]
+   * @param  data - The data used to structure the platoon instance.
+   * @returns the platoon
    */
-  structureData(data) {
-    utils.structureData(this, data, { blacklist: ["badgePath"] });
+  structureData(data: Platoon) : Platoon {
+    utils.structureData(this, data);
+if(data.badgePath){
+    var badgeUrl1: string | Array<string> = data.badgePath;
 
-    var badgeUrl1 = data.badgePath || data.emblemPath;
-
-    if (badgeUrl1) {
+   
       badgeUrl1 = badgeUrl1.split("[FORMAT]").join("png").split("[SIZE]");
 
       this.badge[60] = badgeUrl1.join("60");
       this.badge[320] = badgeUrl1.join("320");
-    }
+    
+  }
     return this;
   }
-
-  async fetch() {
+  /**
+   * Fetch the data of the platoon from Battlelog.
+   * 
+   * @returns The platoon instance
+   */
+  async fetch() : Promise<Platoon> {
     const res = await this.client.axios.get(`/platoon/${this.id}/`);
 
     this.structureData(res.data.context.platoon);
@@ -59,4 +73,3 @@ export class Platoon {
   }
 }
 
-module.exports.Platoon = Platoon;
